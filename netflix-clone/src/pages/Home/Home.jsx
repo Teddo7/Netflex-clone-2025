473b9db9742cc558c9e9d530fed1f9038afb8199
Teddo7 <mercytedd7@gmail.com>
2025-02-10 15:18:14 -0500

    Banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
 
-const Home = ()=> {
+const Home = () => {
   return (
     <>
-    
       <Header />
+      <Banner />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
