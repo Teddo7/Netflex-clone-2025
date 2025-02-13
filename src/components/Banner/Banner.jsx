575806809ad1445fb584ea0a8f10ff0c5e2c8461
Teddo7 <mercytedd7@gmail.com>
2025-02-13 18:24:57 -0500

    intial commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "../../utils/axios";
+import requests from "../..//utils/requests";
+import "./Banner.css";
+
+const Banner = () => {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+    }
+    fetchData().catch((error) => console.log("error", error));
+  }, []);
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">Movie Description</h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </div>
+  );
+};
+
+export default Banner;
