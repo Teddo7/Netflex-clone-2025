3bf73da732c64f6f8d6e18ba17ee30e0b2cd9a48
Teddo7 <mercytedd7@gmail.com>
2025-02-25 18:40:27 -0500

    deploy

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,16 +6,21 @@ import "./Banner.css";
 const Banner = () => {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
-    async function fetchData() {
+   ( async function fetchData() {
+      try { 
       const request = await axios.get(requests.fetchNetflixOriginals);
       setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length)
         ]
       );
-    }
-    fetchData().catch((error) => console.log("error", error));
-  }, []);
+      } catch (error) {
+  console.log("error", error)
+  }})();
+}, []);
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
   return (
     <div
       className="banner"
@@ -27,12 +32,14 @@ const Banner = () => {
       }}
     >
       <div className="banner_contents">
-        <h1 className="banner_title">Movie Name</h1>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">Movie Description</h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
       <div className="banner--fadeBottom"></div>
     </div>
